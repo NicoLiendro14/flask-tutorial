03565665de0fd3e28a9cb6cbc88446fe75aa916e
Nicolas Liendro <nicolasliendro0697@gmail.com>
2022-03-27 16:54:03 -0300

    Fix endpoints and routes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,10 +1,8 @@
-from crypt import methods
 import functools
 
 from flask import(
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
@@ -27,7 +25,7 @@ def register():
             try:
                 db.execute(
                     "INSERT INTO user (username, password) VALUES (?,?)",
-                    (username, generate_password_hash(password)),
+                    (username, password),
                 )
                 db.commit()
             except db.IntegrityError:
@@ -46,12 +44,12 @@ def login():
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username)
+            'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
         if user is None:
             error = 'Incorrect username'
-        elif not check_password_hash(user['password'], password):
+        elif user['password'] != password:
             error = 'Incorrect password'
 
         if error is None:
@@ -69,7 +67,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id)
+            'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
 
